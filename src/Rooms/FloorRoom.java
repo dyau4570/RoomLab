f98958d603da44203ff51ef0c4adc994c1191c7d
BT_1N3_10 <dyau4570@bths.edu>
2018-10-30 11:06:39 -0400

    added a room

---
+++ src/Rooms/FloorRoom.java
@@ -0,0 +1,22 @@
+package Rooms;
+
+import Game.Runner;
+import People.Person;
+
+public class FloorRoom extends Room
+{
+    public FloorRoom(int x, int y) {
+        super(x, y);
+    }
+    /**
+     * Triggers the game ending conditions.
+     * @param x the Person entering
+     */
+    @Override
+    public void enterRoom(Person x) {
+        occupant = x;
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
+        System.out.println("You found the floor room! You can sleep on the floor now.");
+    }
+}
